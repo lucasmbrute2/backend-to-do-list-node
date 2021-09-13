2f5d5dfad7aad2077250c67dd36e8d8cb9ca7852
Lucas <lucasmbrute614@gmail.com>
2021-09-12 21:43:08 -0300

    finalizando endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
 const toDoModel = require('../model/model')
 
 module.exports ={
-    
     get: async (req,res)=>{
         await toDoModel.find({})
     .then(tarefa=>res.status(200).send(tarefa))
-    .catch(err=>console.error(err))
+    .catch(err=>res.status(400).send(err))
     },
     getId: async (req,res)=>{
         await toDoModel.findOne({_id: req.params.id})
@@ -15,13 +14,25 @@ module.exports ={
     post: async (req,res)=>{
         if(req.body && req.body.titulo && req.body.descricao && req.body.prioridade && req.body.estado && req.body.estado){
             await toDoModel.create(req.body)
-            .then(tarefa=>res.status(200).send(tarefa))
+            .then(tarefa=>res.status(201).send(tarefa))
         }else{
-            res.status(400).send('erro')
+            res.status(404).send('erro')
         }
     },
     put: async (req,res)=>{
-        // if(req.body && req.body.titulo && req.body.descricao && req.body.prioridade && req.body.estado && req.body.estado)
-   
+        await toDoModel.updateOne({ _id: req.params.id }, req.body)
+        .then(res.status(200).send('Editado com sucesso!')) 
+        .catch(err=>res.status(404).send(err))
+    },
+
+    deleteId: async (req,res)=>{
+        await toDoModel.deleteOne({ _id: req.params.id })
+        .then(res.status(200).send('Deletado com sucesso!'))
+        .catch(err=>res.status(404).send(err))
+    },
+    deleteAll: async (req,res)=>{
+        await toDoModel.deleteMany({})
+        .then(res.status(200).send('Todos os itens foram apagados.'))
+        .catch(err=>res.status(400).send(err))
     }
 }
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,25 +1,16 @@
 const express  = require("express");
 const router = express.Router()
-const toDoModel = require('../model/model')
 const controller = require('../controllers/index')
 
-router.get('/',controller.get)
+router.get('/',controller.get);
 
-router.get('/:id',controller.getId)
+router.get('/:id',controller.getId);
 
-router.post('/add',controller.post)
+router.post('/add',controller.post);
 
-router.put('/edit/:id', async (req,res)=>{
-    await toDoModel.updateOne({_id: req.params.id}, req.body)
-    .then(()=>res.status(200).send('Tarefa editada com sucesso!'))
-    .catch(err=>res.status(400).send(`${err} Algo deu errado tente novamente.`))
-})
+router.put('/edit/:id',controller.put);
 
-router.delete('/delete/:id', async (req,res)=>{
-    await toDoModel.deleteOne({_id: req.params.id})
-    .then(()=>res.status(200).send('Tarefa apagada com sucesso!'))
-    .catch(err=>res.status(400).send(`${err} Algo deu errado tente novamente.`))
-})
+router.delete('/delete/:id',controller.deleteId);
 
 
 
