1316956c9d2ea86b5224c5ec48005930c7d3b600
Lucas <lucasmbrute614@gmail.com>
2021-09-12 21:47:30 -0300

    setando CORS

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 if(process.env.NODE_ENV!== 'production'){
     require('dotenv').config();
-}
+};
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionSucessStatus: 200
+};
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3003;
-const Connect = require('./connect/connect')
-const db_url = process.env.DB_URL
-const db_user= process.env.DB_USER
-const db_password= process.env.DB_PASS
-const db_data= process.env.DB_DATA
-const tarefaRota = require('./routes/index.routes')
+const Connect = require('./connect/connect');
+const db_url = process.env.DB_URL;
+const db_user= process.env.DB_USER;
+const db_password= process.env.DB_PASS;
+const db_data= process.env.DB_DATA;
+const tarefaRota = require('./routes/index.routes');
 
-Connect(db_url,db_user,db_password,db_data)
+Connect(db_url,db_user,db_password,db_data);
 app.use(express.json());
-app.use('/tarefas',tarefaRota)
-
+app.use('/tarefas',tarefaRota);
+app.use(cors(corsOptions));
 app.get('/',(req,res)=>{
-    res.send('hello')
-})
-app.listen(port,()=>{
+    res.send('Errou o EndPoint ai rs')
+});
+app.listen(process.env.PORT || port,()=>{
     console.info(`Servidor rodando em http://localhost:${port}`)
-})
+});
